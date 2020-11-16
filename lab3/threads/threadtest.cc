54d8668ef6a08f118af83247acdf0983c86ab09f
baker221 <806871923@qq.com>
2020-11-16 23:57:46 +0800

    ex4 finish

--- lab3/threads/threadtest.cc
+++ lab3/threads/threadtest.cc
@@ -11,6 +11,7 @@
 
 #include "copyright.h"
 #include "elevatortest.h"
+#include "synch.h"
 #include "system.h"
 
 // testnum is set in main.cc
@@ -49,15 +50,155 @@ void ThreadTest1() {
   SimpleThread(0);
 }
 
+// begin semaphore reader-writer problem
+// reader first
+int rCnt;
+Semaphore *mutex, *w;
+int data;
+
+void semaphore_reader(int which) {
+  while (true) {
+    mutex->P();
+    rCnt++;
+    if (rCnt == 1) {
+      printf("reader %d is the first reader waiting for reading\n", which);
+      w->P();
+    }
+    mutex->V();
+    // read something here
+    printf("reader %d reads data value is %d\n", which, data);
+    mutex->P();
+    rCnt--;
+    if (rCnt == 0) {
+      w->V();
+      printf("reader %d is the last leaving reader\n", which);
+    }
+    mutex->V();
+  }
+}
+void semaphore_writer(int which) {
+  while (true) {
+    printf("writer %d is waiting for writing\n", which);
+    w->P();
+    // write something here
+    int data_old = data;
+    data += 1;
+    printf("writer %d change data from %d to %d\n", which, data_old, data);
+    w->V();
+    printf("writer %d leaves\n", which);
+  }
+}
+void ThreadTest_Semaphore_rw() {
+  DEBUG('t', "Entering ThreadTest_Semaphore_rw");
+  rCnt = 0;
+  data = 0;
+  mutex = new Semaphore("mutex", 1);
+  w = new Semaphore("w", 1);
+  for (int i = 0; i < 3; i++) {
+    Thread *t = new Thread("reader");
+    t->Fork(semaphore_reader, (void *)i);
+  }
+  for (int i = 0; i < 2; i++) {
+    Thread *t = new Thread("writer");
+    t->Fork(semaphore_writer, (void *)i);
+  }
+}
+// end semaphore reader-writer problem
+
+// begin conditon reader-writer problem
+// reader first
+int AR; // of active readers
+int AW; // of active writers
+int WR; // of waiting readers
+int WW; // of waiting writers
+Lock *lock;
+Condition *okToRead;
+Condition *okToWrite;
+
+void reader(int which) {
+  while (true) {
+    lock->Acquire();
+    while (AW > 0) {
+      printf("reader %d is waiting others writing.\n", which);
+      WR++;
+      okToRead->Wait(lock);
+      WR--;
+    }
+    AR++;
+    lock->Release();
+    printf("reader reads data value is %d\n", data);
+    lock->Acquire();
+    AR--;
+    if (((AR + WR) == 0) && (WW > 0)) {
+      printf("reader %d is the last leaving reader.\n", which);
+      okToWrite->Signal(lock);
+    }
+    lock->Release();
+  }
+}
+void writer(int which) {
+  while (true) {
+    lock->Acquire();
+    while ((AR + AW) > 0) {
+      printf("writer %d is waiting others reading/writing.\n", which);
+      WW++;
+      okToWrite->Wait(lock);
+      WW--;
+    }
+    AW++;
+    lock->Release();
+    int data_old = data;
+    data += 1;
+    printf("writer change data from %d to %d\n", data_old, data);
+    lock->Acquire();
+    AW--;
+    if (WR > 0) {
+      printf("writer %d is trying to wake up a reader\n", which);
+      okToRead->Signal(lock);
+    } else if (WW > 0) {
+      printf("writer %d is trying to wake up a writer\n", which);
+      okToWrite->Signal(lock);
+    }
+    lock->Release();
+  }
+}
+void ThreadTest_rw() {
+  DEBUG('t', "Entering ThreadTest_rw");
+  data = 0;
+  AR = 0;
+  AW = 0;
+  WR = 0;
+  WW = 0;
+  lock = new Lock("lock");
+  okToRead = new Condition("okToRead");
+  okToWrite = new Condition("okToWrite");
+  for (int i = 0; i < 1; i++) { // reader num
+    Thread *t = new Thread("reader");
+    t->Fork(reader, (void *)i);
+  }
+  for (int i = 0; i < 2; i++) {
+    Thread *t = new Thread("writer");
+    t->Fork(writer, (void *)i);
+  }
+}
+// end conditon reader-writer problem
+
 //----------------------------------------------------------------------
 // ThreadTest
 // 	Invoke a test routine.
 //----------------------------------------------------------------------
 
 void ThreadTest() {
+  // DebugInit("t");
   switch (testnum) {
   case 1:
     ThreadTest1();
+    break;
+  case 2:
+    ThreadTest_Semaphore_rw();
+    break;
+  case 3:
+    ThreadTest_rw();
     break;
   default:
     printf("No test specified.\n");
